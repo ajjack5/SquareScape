1e8aedbf87e3927663c753e56506b22dd24961d4
andrew.joseph.jack <andrew.joseph.jack@gmail.com>
2019-07-11 20:13:59 +1000

    Created solution

---
+++ SquareScape/SquareScape.Terminal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SquareScape.Terminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
