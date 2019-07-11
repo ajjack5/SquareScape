ec267eb17e75bd9eae899cc2de924a5d542bc224
andrew.joseph.jack <andrew.joseph.jack@gmail.com>
2019-07-11 22:24:59 +1000

    slight changes to queue

--- SquareScape/SquareScape.Commands/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace SquareScape.Commands
-{
-    public class Class1
-    {
-    }
-}
---
+++ SquareScape/SquareScape.Commands/Commands/PositionUpdate.cs
@@ -0,0 +1,7 @@
+﻿namespace SquareScape.Commands.Commands
+{
+    public class PositionUpdate : IGameUpdate
+    {
+        // set X / Y position here if changed ??
+    }
+}
