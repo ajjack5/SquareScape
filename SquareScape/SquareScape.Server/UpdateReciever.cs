2e66bf1342432a3e6c51018eac2b1e1c0356b2b9
Michael Stagg <michael.lee.stagg@dxc.com>
2019-07-11 22:35:52 +0930

    intial commit

---
+++ SquareScape/SquareScape.Server/UpdateReciever.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace SquareScape.Server
+{
+    class UpdateReciever
+    {
+        public void UpdateReciever(TcpClient client)
+        {
+            NetworkStream strm = client.getStream();
+            IFormatter formatter = new BinaryFormatter();
+
+            while (strm)
+            {
+                object o = (object)formatter.Deserialize(strm);
+            }
+        }
+    }
+}
